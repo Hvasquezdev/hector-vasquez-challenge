4a9328f389353bccd4d93c60594d0df61e4bc2d5
Hector Vazquez <hvasquezdev@gmail.com>
2022-10-19 20:55:54 -0300

    :sparkles: add base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  isSelected: Boolean,
+  isDisabled: Boolean,
+});
+
+const classNames = computed(() => ({
+  "is-selected": props.isSelected,
+  "is-disabled": props.isDisabled,
+}));
+</script>
+
+<template>
+  <button class="base-button" :class="classNames">
+    <slot />
+  </button>
+</template>
+
+<style>
+.base-button {
+  width: 100%;
+  border-radius: var(--button-border-radius);
+  background-color: transparent;
+  padding: 5px 19px;
+  font-family: var(--ff-popins);
+  font-size: var(--fs-s);
+  font-weight: var(--fw-bold);
+  color: var(--purple);
+  border: 2px solid var(--purple-light);
+  min-height: 56px;
+  cursor: pointer;
+  transition: all 0.25s;
+}
+
+.base-button.is-selected {
+  background-color: var(--orange);
+  color: var(--white);
+  border-color: var(--orange);
+}
+
+.base-button.is-disabled {
+  opacity: 0.6;
+  pointer-events: none;
+  user-select: none;
+}
+</style>
