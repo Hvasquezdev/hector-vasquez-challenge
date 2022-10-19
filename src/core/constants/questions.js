266a86e76112007c6d09d392b390284035cc3b8d
Hector Vazquez <hvasquezdev@gmail.com>
2022-10-19 19:01:30 -0300

    :sparkles: add questions list

---
+++ src/core/constants/questions.js
@@ -0,0 +1,107 @@
+export const QUESTIONS = [
+  {
+    label: "Which is the largest country in the world by population?",
+    answers: [
+      {
+        label: "India",
+        isCorrect: false,
+      },
+      {
+        label: "USA",
+        isCorrect: false,
+      },
+      {
+        label: "China",
+        isCorrect: true,
+      },
+      {
+        label: "Russia",
+        isCorrect: false,
+      },
+    ]
+  },
+  {
+    label: "When did the second world war end?",
+    answers: [
+      {
+        label: "1945",
+        isCorrect: true,
+      },
+      {
+        label: "1939",
+        isCorrect: false,
+      },
+      {
+        label: "1944",
+        isCorrect: false,
+      },
+      {
+        label: "1942",
+        isCorrect: false,
+      },
+    ]
+  },
+  {
+    label: "Which was the first country to issue paper currency?",
+    answers: [
+      {
+        label: "USA",
+        isCorrect: false,
+      },
+      {
+        label: "France",
+        isCorrect: false,
+      },
+      {
+        label: "Italy",
+        isCorrect: false,
+      },
+      {
+        label: "China",
+        isCorrect: true,
+      },
+    ]
+  },
+  {
+    label: "Which city hosted the 1996 Summer Olympics?",
+    answers: [
+      {
+        label: "Atlanta",
+        isCorrect: true,
+      },
+      {
+        label: "Sydney",
+        isCorrect: false,
+      },
+      {
+        label: "Athens",
+        isCorrect: false,
+      },
+      {
+        label: "Beijing",
+        isCorrect: false,
+      },
+    ]
+  },
+  {
+    label: "Who invented the telephone?",
+    answers: [
+      {
+        label: "Albert Einstein",
+        isCorrect: false,
+      },
+      {
+        label: "Alexander Graham Bell",
+        isCorrect: true,
+      },
+      {
+        label: "Isaac Newton",
+        isCorrect: false,
+      },
+      {
+        label: "Marie Curie",
+        isCorrect: false,
+      },
+    ]
+  },
+];
