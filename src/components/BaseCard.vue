31762a386c93d549a3116abba9e4d69bc17e630a
Hector Vazquez <hvasquezdev@gmail.com>
2022-10-19 21:55:10 -0300

    :sparkles: add base card component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="base-card">
+    <slot />
+  </div>
+</template>
+
+<style>
+.base-card {
+  padding: 32px;
+  border-radius: 24px;
+  background-color: #fff;
+  width: 100%;
+  max-width: 468px;
+}
+</style>
