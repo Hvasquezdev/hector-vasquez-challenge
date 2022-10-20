e4ea3e0029a2af938869de0f2b5e81e493001fd0
Hector Vazquez <hvasquezdev@gmail.com>
2022-10-19 21:55:21 -0300

    :sparkles: add base title component

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="base-title">
+    <slot />
+  </h1>
+</template>
+
+<style>
+.title-text {
+  color: #2c3e50;
+  font-size: 24px;
+  font-weight: 700;
+  text-align: center;
+}
+</style>
